263b8f9cc12a46a170bbfd3402fac49b6090f466
dbzahariev <dbzahariev@gmail.com>
2019-01-23 19:12:48 +0200

    Add new files

--- ex_01_list_and_dictionaries/dictionaries/04_forum_topics.py
+++ ex_01_list_and_dictionaries/dictionaries/04_forum_topics.py
@@ -1,4 +1,4 @@
-my_dict = {}
+forum_dict = {}
 
 data = input()
 while not data.lower() == 'filter':
@@ -11,30 +11,9 @@ while not data.lower() == 'filter':
         if hashtag not in forum_dict[topic]:
             forum_dict[topic].append(hashtag)
 
-<<<<<<< HEAD
     data = input()
 
 target_topic_set = set(input().split(', '))
 for topic, hashtag in forum_dict.items():
     if target_topic_set.issubset(set(hashtag)):
         print(f"{topic} | {', '.join(list(map(lambda x: '#' + x, hashtag)))}")
-=======
-while True:
-    internal = input()
-    if internal.lower() == 'filter':
-        target_topic = input().split(', ')
-        for key, value in my_dict.items():
-            if len(value) > 0 and is_contains(target_topic, value):
-                value = add_prefix('#', value)
-                print(f"{key} | {', '.join(value)}")
-        break
-    pair = internal.split(' -> ')
-    key = pair[0]
-    values = pair[1].split(', ')
-    if key not in my_dict:
-        my_dict[key] = []
-
-    for value in values:
-        if value not in my_dict[key]:
-            my_dict[key].append(value)
->>>>>>> parent of b7388fb... Add new files
